556fedb94562f668b4fe1278d7f7a2a1200eb59c
MickeyLer <63302577+MickeyLer@users.noreply.github.com>
2020-04-16 15:05:33 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,49 +1,7 @@
 <?php
-function processMessage($update) {
-    if($update["queryResult"]["action"] == "sayHello"){
-        sendMessage(array(
-            "source" => $update["responseId"],
-            "fulfillmentText"=>"Hello from webhook",
-            "payload" => array(
-                "items"=>[
-                    array(
-                        "simpleResponse"=>
-                    array(
-                        "textToSpeech"=>"response from host"
-                         )
-                    )
-                ],
-                ),
-           
-        ));
-    }
-}
- 
-function sendMessage($parameters) {
-    echo json_encode($parameters);
-}
- 
 $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
 if (isset($update["queryResult"]["action"])) {
-    processMessage($update);
-}else{
-     sendMessage(array(
-            "source" => $update["responseId"],
-            "fulfillmentText"=>"Hello from webhook",
-            "payload" => array(
-                "items"=>[
-                    array(
-                        "simpleResponse"=>
-                    array(
-                        "textToSpeech"=>"Bad request"
-                         )
-                    )
-                ],
-                ),
-           
-        ));
+   echo "hello world";
 }
-
-
 ?>
